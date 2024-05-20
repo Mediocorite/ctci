ec2b9c1af71e31d5bc145cfe22ca19eac8aec6f8
Anzar Ahmad Malik <12586091+Mediocorite@users.noreply.github.com>
2024-05-20 13:46:36 +0100

    Init commit

---
+++ 01.arrays_and_strings-CTCI/isUnique.java
@@ -0,0 +1,12 @@
+public class isUnique {
+  // Question: Implement an algorithm to deternmine if a string has all unique characters. What if you cannot use additional data structures.
+  //
+  // Interviewer Questions -> You should generally make it a habbit to ask the interviewer w
+
+
+  String input = "abcde";
+
+  public boolean isUniqueCheck(String s) {
+    return false;
+  }
+}
